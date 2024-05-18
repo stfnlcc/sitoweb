c23ee02ba1c061cbf0ae07568df300e363c17fb9
stfnlcc <stefano.locci@bootcamp-upgrade.com>
2024-05-18 20:09:45 +0200

    ok

---
+++ sitoweb/src/App.jsx
@@ -0,0 +1,17 @@
+import marcoLogo from "/logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a></a>
+        <a href="index.html" target="_blank">
+          <img src={marcoLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
